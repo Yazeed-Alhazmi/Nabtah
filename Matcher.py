0432137e1019a5094c881cc53c1e69299471191a
Yaz070 <167567271+Yaz070@users.noreply.github.com>
2025-01-30 18:48:42 +0300

    Add files via upload

---
+++ Matcher.py
@@ -0,0 +1,148 @@
+import spacy
+from spacy.matcher import Matcher
+from transformers import pipeline
+import torch
+import json # to read the patterns json file
+import plotly.graph_objects as go # for Radar Chart
+
+
+# loading the patterns from patterns.json file
+with open("patterns.json", 'r') as patterns_json:
+    patterns = json.load(patterns_json)
+
+nlp = spacy.load('en_core_web_lg')
+
+# BERT
+sentiment_analyzer = pipeline('sentiment-analysis', model="nlptown/bert-base-multilingual-uncased-sentiment")
+
+# Matcher 
+matcher = Matcher(nlp.vocab) # Initializing the Matcher
+
+for pattern_label, pattern in patterns.items():
+    matcher.add(pattern_label, pattern)
+
+# %%
+
+# Apply matcher with BERT sentiment analysis
+class Analyze:
+
+    def __init__(self):
+        self.track_scores = {}
+        self.RIASEC_scores = {}
+        self.total_score = {}
+
+    def analyze_conversation(self, conversation):
+        doc = nlp(conversation)
+        matches = matcher(doc)
+
+        # Tracks scores
+        track_scores = {
+            "General": 0,
+            "Computer Science and Engineering": 0,
+            "Health and Life Sciences": 0,
+            "Business Administration": 0,
+            "Sharia": 0,
+
+            "Realistic": 0,
+            "Investigative": 0,
+            "Artistic": 0,
+            "Social": 0,
+            "Enterprising": 0,
+            "Conventional": 0
+        }
+
+        for match_id, start, end in matches:
+            # matched track label
+            track_label = nlp.vocab.strings[match_id]
+
+            # Look at 3 words before and after the keyword for sentiment analysis
+            keyword_context = doc[max(0, start-3):min(len(doc), end+3)]
+            context_text = keyword_context.text
+
+            # BERT based sentiment analysis on the context
+            sentiment_result = sentiment_analyzer(context_text)[0]  # sentiment result
+
+            # Scoring based on BERT sentiment label
+            if sentiment_result['label'] in ['4 stars', '5 stars']:  # Positive sentiment
+                track_scores[track_label] += 1
+            elif sentiment_result['label'] in ['1 star', '2 stars']:  # Negative sentiment
+                track_scores[track_label] -= 1
+            else:  
+                track_scores[track_label] += 1  # neutral as positive
+
+        track_scores = list(track_scores.items())
+        return dict(track_scores[:5]), dict(track_scores[5:]) # (High school tracks score, RIASEC codes score)    
+
+    def analyze(self, conversation):
+
+        self.track_scores, self.RIASEC_scores = self.analyze_conversation(conversation)
+
+        if self.track_scores[max(self.track_scores, key=self.track_scores.get)] + self.track_scores[min(self.track_scores, key=self.track_scores.get)] == 0:
+            return {"status": "warning", "message": "It seems the assessment was not completed. Please retake the assessment"}
+        if self.RIASEC_scores[max(self.RIASEC_scores, key=self.RIASEC_scores.get)] + self.RIASEC_scores[min(self.RIASEC_scores, key=self.RIASEC_scores.get)] == 0:
+            return {"status": "warning", "message": "It seems the assessment was not completed. Please retake the assessment"}
+
+        # Calculating the total score
+        with open("track_to_RIASEC.json", "r") as track_to_RIASEC_json:
+            track_to_RIASEC = json.load(track_to_RIASEC_json)
+
+        for track, RIASEC_Codes in track_to_RIASEC.items():
+            self.total_score[track] = self.track_scores[track] + 0.5 * sum(self.RIASEC_scores[RIASEC_Code] for RIASEC_Code in RIASEC_Codes) # 50% of RAISEC score affect the total score
+
+        # returning recommended track
+        max_track = max(self.total_score, key=self.total_score.get)
+        return {"status": "success", "message": "Recommended Track: " + max_track}
+
+    # Radar Chart
+    def radarChart(self, RIASEC=False, original=False, normalize=True): 
+        
+        scores = self.total_score
+        title='High School Track Recommendation'
+
+        if RIASEC == True:
+            scores = self.RIASEC_scores
+            title = 'Holland Codes Assessment'
+        if original == True:
+            scores = self.track_scores
+            title='Original Scores'
+
+        # converting the scores into lists so we can sort them for better appearance
+        tracks = list(scores.keys())
+        scores = list(scores.values())
+        sorted_data = sorted(zip(scores, tracks), reverse=True)
+        sorted_scores, sorted_tracks = zip(*sorted_data)
+
+        if normalize:
+            min_score = min(sorted_scores)
+            max_score = max(sorted_scores)
+            print("max = ", max_score)
+            print("min = ", min_score)
+            scores = [(score - min_score) / (max_score - min_score) for score in sorted_scores]
+            scores = [round(score, 3) for score in scores] # ".3f"
+
+        fig = go.Figure()
+
+        fig.add_trace(
+            go.Scatterpolar(
+            r=scores,
+            theta=sorted_tracks,
+            fill='toself',
+            name=title,
+            fillcolor='#a8d4b4',
+            line_color="#0e6926",
+            opacity=0.5
+        )
+        )
+
+
+        fig.update_layout(
+            polar=dict(
+                radialaxis=dict(
+                    visible=True,
+                    range=[0, 1] if normalize else [0, max(scores)]
+                )),
+            title=title+" Radar Chart",
+            showlegend=False
+        )
+
+        return fig
---
+++ Nabtah.py
@@ -0,0 +1,154 @@
+import streamlit as st
+import openai
+from projectM_matcher_V2 import Analyze
+
+st.set_page_config(page_title="Nabtah", page_icon="🌿", layout="centered")
+
+st.logo("Medvation_logo.png", size='large', link='https://www.medvation.net')
+
+st.html("""
+        <style>
+            [alt=Logo] {
+            height: 4rem;
+            }
+        </style>
+    """)
+
+
+client = openai.OpenAI(api_key=st.secrets["OPENAI_API_KEY"]) # Opeai API Key
+# LLM Model
+if "openai_model" not in st.session_state:
+    st.session_state["openai_model"] = "gpt-4o"
+
+# Chat history
+if "texts" not in st.session_state:
+    st.session_state.texts = []
+
+
+if "page" not in st.session_state: # we have two pages. Chat page and Recommendation page
+    st.session_state.page = "Chat" 
+
+if st.session_state.page == "Chat": # Chat Page
+
+    # title
+    st.markdown("<h1 style='text-align: center; color: #A8D4B4;'>Nabtah 🌿</h1>", unsafe_allow_html=True) 
+
+    # Initial system message
+    if not st.session_state.texts:
+        st.session_state.texts.append({
+            "role": "system",
+            "content": """
+                        You are a friendly chatbot engaging in a casual conversation with a teenager. Your goal is to naturally guide the conversation towards their daily life, interests, and personal preferences in a way that feels effortless and organic, while subtly exploring aspects of the Holland Codes (RIASEC) without making it obvious.
+
+                        Conversation Guidelines:
+                        1. Start with a friendly greeting – Ask how their day is going in a warm and casual way.
+                        2. Encourage them to talk about recent activities – Ask about fun things they've been doing, shows they've watched, or hobbies they've enjoyed.
+                        3. Casually introduce school subjects – Inquire about subjects they find interesting or enjoyable, without focusing on grades.
+                        4. Explore their free time activities and hobbies – Encourage them to share how they spend their spare time, such as sports, arts, gaming, or volunteering.
+                        5. Subtly inquire into their Holland Code traits:
+                        - Realistic (R): "Do you enjoy hands-on activities like building, fixing, or exploring outdoors?"
+                        - Investigative (I): "Have you found yourself curious about how things work or why things happen?"
+                        - Artistic (A): "Do you like expressing yourself through art, music, or writing?"
+                        - Social (S): "Do you enjoy helping others or working in a team?"
+                        - Enterprising (E): "Do you like leading projects or convincing people of new ideas?"
+                        - Conventional (C): "Do you find organizing tasks or planning events satisfying?"
+                        6. Prompt them about future interests – Ask if there's anything they’re excited to learn or try, ensuring it doesn’t feel like a career discussion.
+                        7. Understand their preferred work style – Find out if they enjoy working alone, in groups, or through hands-on projects by asking about recent activities they enjoyed.
+
+                        Tone & Restrictions:
+                        - Keep the conversation fun, light, and pressure-free.
+                        - Maintain a friendly, conversational tone, making the teen feel comfortable and understood.
+
+                        Your role is to engage in an enjoyable conversation while subtly understanding their preferences aligned with the Holland Codes, without tracking or analyzing their responses.
+                        """
+        })
+
+    # Display History on each rerun
+    for text in st.session_state.texts[1:]:
+        with st.chat_message(text["role"]):
+            st.markdown(text["content"])
+
+
+    user_prompt = st.chat_input("How is your day going?")
+    if user_prompt:
+        
+        with st.chat_message("user"): 
+            st.markdown(user_prompt) # Display User Input   
+        st.session_state.texts.append({"role": "user", "content": user_prompt}) # add user input to history
+
+        # Assistant (AI Model):  
+        with st.chat_message("assistant"):
+            model_respone =  client.chat.completions.create(
+                model=st.session_state["openai_model"],
+                messages = [{"role": m["role"], "content":m["content"]} for m in st.session_state.texts],
+                stream=True
+            )
+            response = st.write_stream(model_respone) # Display mode response in stream form
+        st.session_state.texts.append({"role":"assistant","content":response}) # add model response to history
+
+    with st.sidebar:
+
+        st.sidebar.markdown("### View Your Career Recommendation")
+        st.sidebar.markdown("Click the button below to see your recommended career track!")
+        # Button to switch to the Recommendations page
+        if st.button("Recommendation"):
+            st.session_state.page = "Recommendation"
+
+
+
+
+elif st.session_state.page == "Recommendation": # Recommendations page
+    
+    # collect user inputs from the conversation
+    user_inputs = []
+    for txt in st.session_state.texts:
+        if txt['role'] == 'user':
+            user_inputs.append(txt['content'])
+    user_input = ". ".join(user_inputs)
+
+
+    thx_msg =  client.chat.completions.create(
+        model=st.session_state["openai_model"],
+        messages = st.session_state.texts + [{"role": "system", "content": "Please provide a positive and encouraging message to the teen. It will be used in the final reccomendation report."}],
+    )
+
+    # Displaying options based on their interests and hobbies
+    actions =  client.chat.completions.create(
+        model=st.session_state["openai_model"],
+        messages = st.session_state.texts + [{"role": "system", "content": "Please Provide suggestions for next steps, like what the teen can explore further or ways to develop their skills in a friendly way. It will be used in the final reccomendation report."}],
+    )
+
+    thx_msg = thx_msg.choices[0].message.content
+    actions = actions.choices[0].message.content
+
+
+    analyzeConv = Analyze() 
+    result = analyzeConv.analyze(user_input) # Analyzing the conversation
+    if result['status']=='warning': # Display a warning messgae if the assesment is not completed
+        st.warning(result['message'])
+        st.stop()
+    result = result['message']
+
+
+    st.title("Here's the Results 🥳") # Recommendation page title
+
+    st.write(thx_msg)
+
+    original_fig = analyzeConv.radarChart(original=True) # Radar chart using the original scores
+    st.plotly_chart(original_fig)
+
+    RIASEC_fig = analyzeConv.radarChart(RIASEC=True) # Radar chart using the RIASEC scores
+    st.plotly_chart(RIASEC_fig)
+
+    total_fig = analyzeConv.radarChart() # Radar chart using the original+RIASEC scores
+    st.plotly_chart(total_fig)
+
+    # Recommended track
+    st.markdown(f"<span style='color:#6b9080; font-weight:bold; font-size:25px'>{result}</span>", unsafe_allow_html=True) 
+
+    st.write(actions)
+
+
+
+    
+   
